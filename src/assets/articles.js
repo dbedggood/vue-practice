017381da28493a67fda3df0e983be6851e830ac2
Daniel Bedggood <ddbedggood@gmail.com>
2019-09-07 14:25:34 +1200

    Add smaller images for lazy loading

--- src/assets/articles.js
+++ src/assets/articles.js
@@ -5,6 +5,7 @@ export default [
     text:
       "This website was made to improve my CSS and Vue.js skills, especially regarding the creation of responsive websites. If you're on a computer, try resizing the window to see how the layout changes! I'm am very happy with how this turned out.",
     imagePath: require('../assets/matapouri.png'),
+    imagePathFullSize: require('../assets/matapouri-large.png'),
     imageAlt: 'A girl standing on rocky cliff, sky in background. (Photograph)',
     info: 'August 23rd, 2019'
   },
@@ -14,6 +15,7 @@ export default [
     text:
       'The Recent Posts and About Me pages each only took a couple minutes to make after I finished the CSS and components for the Home page. Really demonstrates how versatile modular code can be when written well.',
     imagePath: require('../assets/tauranga.png'),
+    imagePathFullSize: require('../assets/tauranga-large.png'),
     imageAlt:
       'A girl in a red swimsuit walking in shallow water at the beach, an island in the distance. (Photograph)',
     info: 'August 17th, 2019'
@@ -24,6 +26,7 @@ export default [
     text:
       'Quisque at tincidunt massa. Sed id euismod nulla. Phasellus vel turpis a mi auctor sodales. Aliquam vel congue tellus, ut interdum enim. Aenean vitae erat dictum, pharetra leo a, condimentum ligula. Aliquam mauris erat, egestas ut tristique nec, tincidunt ut leo. Nam nec consectetur lorem.',
     imagePath: require('../assets/whangateau.png'),
+    imagePathFullSize: require('../assets/whangateau-large.png'),
     imageAlt:
       'The sun setting over a landscape of grassy hills and trees. (Photograph)',
     info: 'August 8th, 2019'
@@ -34,6 +37,7 @@ export default [
     text:
       'Morbi sit amet nisi vestibulum, aliquet ante ut, fermentum risus. Mauris ac diam eros. Curabitur elementum dignissim mattis. Aliquam laoreet, ligula accumsan ultricies condimentum, dolor massa maximus dui, ut convallis sapien nibh sed quam.',
     imagePath: require('../assets/russel.png'),
+    imagePathFullSize: require('../assets/russel-large.png'),
     imageAlt:
       'A rocky island with a tunnel-like hole underneath it large enough for boats to pass through. (Photograph)',
     info: 'July 29th, 2019'
